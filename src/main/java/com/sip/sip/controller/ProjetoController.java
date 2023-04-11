42c7bf83031f66c6a7621b06f0c244d61fc060f8
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-04-10 22:52:48 -0300

    fix: separate use case controller from rest controller

---
+++ src/main/java/com/sip/sip/controller/ProjetoController.java
@@ -0,0 +1,46 @@
+package com.sip.sip.controller;
+
+import com.sip.sip.exception.ProjetoNotFoundException;
+import com.sip.sip.model.Projeto;
+import com.sip.sip.service.IManterProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projetos")
+public class ProjetoController {
+
+    @Autowired
+    private IManterProjetoService manterProjetoService;
+
+
+
+    @GetMapping("")
+    public List<Projeto> listarProjetos() {
+        return manterProjetoService.listarProjetos();
+    }
+
+    @GetMapping("/{id}")
+    public Projeto buscarProjetoPorId(@PathVariable Long id) throws ProjetoNotFoundException {
+        return manterProjetoService.buscarProjetoPorId(id);
+    }
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Projeto criarProjeto(@RequestBody Projeto projeto) {
+        return manterProjetoService.criarProjeto(projeto);
+    }
+
+    @PutMapping("/{id}")
+    public Projeto atualizarProjeto(@PathVariable Long id, @RequestBody Projeto projeto) throws ProjetoNotFoundException {
+        return manterProjetoService.atualizarProjeto(id, projeto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirProjeto(@PathVariable Long id) throws ProjetoNotFoundException {
+        manterProjetoService.excluirProjeto(id);
+    }
+}
