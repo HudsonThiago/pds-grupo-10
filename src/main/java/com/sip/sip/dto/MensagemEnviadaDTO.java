f26273c940d7ea7f7144dc33d7adb0ec75772b3c
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2023-05-13 15:21:16 -0300

    fix: showing all messages

--- src/main/java/com/sip/sip/dto/MensagemEnviadaDTO.java
+++ src/main/java/com/sip/sip/dto/MensagemEnviadaDTO.java
@@ -4,10 +4,20 @@ package com.sip.sip.dto;
 public class MensagemEnviadaDTO {
     private String conteudo;
     private Long usuarioDestinatario;
+    private Long usuarioRemetente;
 
     public MensagemEnviadaDTO(String conteudo, Long usuarioRemetente, Long usuarioDestinatario) {
         this.conteudo = conteudo;
         this.usuarioDestinatario = usuarioDestinatario;
+        this.usuarioRemetente = usuarioRemetente;
+    }
+
+    public Long getUsuarioRemetente() {
+        return usuarioRemetente;
+    }
+
+    public void setUsuarioRemetente(Long usuarioRemetente) {
+        this.usuarioRemetente = usuarioRemetente;
     }
 
     public String getConteudo() {
